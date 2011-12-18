0021ea80bf3837e54f8d41e846f7b0b4a3097813
Mohammad J. Fathinia <mj.fathinia@gmail.com>
2011-12-18 11:28:41 +0400

    Added an extension to string object to simply parse item

---
+++ StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Drawing;
+using System.IO;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using Cheraq.Utilities;
+
+namespace Cheraq.Extensions
+{
+    /// <summary>
+    /// Contains list of usefull extensions for String type
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Parses this string and returns equivalent dynamic object, always check for null values :)
+        /// </summary>
+        /// <param name="phrase"></param>
+        /// <returns></returns>
+        public static dynamic ParseJSON(this string phrase)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            jss.RegisterConverters(new JavaScriptConverter[] { new DynamicJsonConverter() });
+            dynamic obj = jss.Deserialize(phrase, typeof(object));
+            return obj;
+        }
+
+    }
+}
