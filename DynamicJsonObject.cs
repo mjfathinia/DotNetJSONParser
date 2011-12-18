6a75e5d46f799606f3d60276d8aa08b3c91db3de
Mohammad J. Fathinia <mj.fathinia@gmail.com>
2011-12-18 11:13:50 +0400

    First commit

---
+++ DynamicJsonObject.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Web.Script.Serialization;
+
+namespace Cheraq.Utilities
+{
+    /// <summary>
+    /// dynamic decoder adopted from Shawn Weisfeld, http://bit.ly/jPqVsQ
+    /// </summary>
+    public sealed class DynamicJsonObject: DynamicObject
+    {
+        private readonly IDictionary<string, object> _dictionary;
+
+        public DynamicJsonObject(IDictionary<string, object> dictionary)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+            _dictionary = dictionary;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (!_dictionary.TryGetValue(binder.Name, out result))
+            {
+                // return null to avoid exception.  caller can check for null this way...
+                result = null;
+                return true;
+            }
+
+            var dictionary = result as IDictionary<string, object>;
+            if (dictionary != null)
+            {
+                result = new DynamicJsonObject(dictionary);
+                return true;
+            }
+
+            var arrayList = result as ArrayList;
+            if (arrayList != null && arrayList.Count > 0)
+            {
+                if (arrayList[0] is IDictionary<string, object>)
+                    result = new List<object>(arrayList.Cast<IDictionary<string, object>>().Select(x => new DynamicJsonObject(x)));
+                else
+                    result = new List<object>(arrayList.Cast<object>());
+            }
+
+            return true;
+        }
+    }
+
+    public sealed class DynamicJsonConverter : JavaScriptConverter
+    {
+        public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+
+            return type == typeof(object) ? new DynamicJsonObject(dictionary) : null;
+        }
+
+        public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
+        {
+            return null;
+        }
+
+        public override IEnumerable<Type> SupportedTypes
+        {
+            get { return new ReadOnlyCollection<Type>(new List<Type>(new Type[] { typeof(object) })); }
+        }
+    }
+}
